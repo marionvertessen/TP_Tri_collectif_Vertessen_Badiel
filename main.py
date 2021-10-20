96282e3e688e89553990d38ce2a514d3c74d08c8
marionvertessen <st.marion42@gmail.com>
2021-10-20 10:25:58 +0200

    Test

--- main.py
+++ main.py
@@ -73,5 +73,23 @@ for i in range (4):
     affichageAgent(listeAgent)
 
 
+# importations à faire pour la réalisation d'une interface graphique
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
 
+# Première étape : création d'une application Qt avec QApplication
+#    afin d'avoir un fonctionnement correct avec IDLE ou Spyder
+#    on vérifie s'il existe déjà une instance de QApplication
+app = QApplication.instance()
+if not app: # sinon on crée une instance de QApplication
+    app = QApplication(sys.argv)
+
+# création d'une fenêtre avec QWidget dont on place la référence dans fen
+fen = QWidget()
+
+# la fenêtre est rendue visible
+fen.show()
+
+# exécution de l'application, l'exécution permet de gérer les événements
+app.exec_()
 
